eacae48feba38af4f533110786cc1ff15cc9e0ab
aabewhite <aabewhite@users.noreply.github.com>
2022-12-31 03:19:48 +0000

    deploy: f0b162eb4d772aabd96a8daf4fcbbc2095bd1bd3

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
         .library(name: "JXSwiftUI", targets: ["JXSwiftUI"]),
     ],
     dependencies: [ .package(name: "swift-docc-plugin", url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"), 
-        .package(url: "https://github.com/jectivex/JXBridge.git", from: "0.1.12"),
-        .package(url: "https://github.com/jectivex/JXKit.git", from: "3.3.4"),
+        .package(url: "https://github.com/jectivex/JXBridge.git", from: "0.1.13"),
+        .package(url: "https://github.com/jectivex/JXKit.git", from: "3.4.0"),
     ],
     targets: [
         .target(name: "JXSwiftUI", dependencies: ["JXBridge", "JXKit"]),
